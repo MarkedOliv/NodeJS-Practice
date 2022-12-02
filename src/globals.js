49039238a9c9cf8aca11d481962b81c03ff9fe07
MarkedOlive <olivdev4@gmail.com>
2022-12-02 19:25:27 -0400

    Main global objects

---
+++ src/globals.js
@@ -0,0 +1,7 @@
+// There are some global objects that have already been defined by default.
+
+console.log("Dirname: ", __dirname);
+console.log("Filename: ", __filename);
+console.log("Module: ", module);
+console.log("Require: ", require);
+console.log("Process: ", process);
