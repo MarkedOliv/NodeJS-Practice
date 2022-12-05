8e76dadba564487a174280862d9a410898de0a0f
MarkedOlive <olivdev4@gmail.com>
2022-12-05 19:35:24 -0400

    Creating module math

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@
 
 const { myWebAddress, myEmail, myName } = require("./module/myModule");
 
-console.log(myWebAddress, myEmail, myName)
+console.log(myWebAddress, myEmail, myName);
+
+
+// Require math
+const math = require('./module/math/index');
+
+console.log(math.add(10, 10));
+console.log(math.subtract(10, 10));
+console.log(math.multiply(10, 10));
+console.log(math.divide(10, 10));
