2a47f701184a0bb2736284c8431e4faf16a8a37c
MarkedOlive <olivdev4@gmail.com>
2022-12-06 16:32:25 -0400

    Understanding "fs"  module with async code

--- src/nodeApiModules/fs.js
+++ src/nodeApiModules/fs.js
@@ -1,15 +1,28 @@
 // The fs module enables interacting with the file system in a way modeled on standard POSIX functions.
 const fs = require('fs');
 
-const first = fs.readFileSync('./data/first.txt', 'utf-8');
-const second = fs.readFileSync('./data/second.txt', 'utf-8');
+// Async Code:
+fs.readFile('./src/nodeApiModules/data/first.txt', (err, data) => {
+    if (err) console.log(err)
+    console.log(data.toString())
 
-console.log(first);
-console.log(second);
-
-fs.writeFileSync('./data/third.txt', 'This is a third file');
-
-const title = 'This is a title for fourth.txt'
-fs.writeFileSync('./data/fourth.txt', title, {
-    flag: 'a'
+    fs.writeFile('./src/nodeApiModules/data/newFile.txt', 'Created from fs.js', (err, data) => {
+        console.log(err);
+        console.log(data);
+    })
 })
+
+
+// Sync Code:
+// const first = fs.readFileSync('./src/nodeApiModules/data/first.txt', 'utf-8');
+// const second = fs.readFileSync('./src/nodeApiModules/data/second.txt', 'utf-8');
+
+// console.log(first);
+// console.log(second);
+
+// fs.writeFileSync('./src/nodeApiModules/data/third.txt', 'This is a third file');
+
+// const title = 'This is a title for fourth.txt'
+// fs.writeFileSync('./src/nodeApiModules/data/fourth.txt', title, {
+//     flag: 'a'
+// })
