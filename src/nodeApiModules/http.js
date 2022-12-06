5816f4d6aa4340e941ccb7b3fc83d3deccc5422b
MarkedOlive <olivdev4@gmail.com>
2022-12-06 17:04:09 -0400

    Working requests with the module "http"

--- src/nodeApiModules/http.js
+++ src/nodeApiModules/http.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
-http.createServer((req, res) => {
-    res.write('Hello World');
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/') {
+        res.write('<h1>Welcome to the server</h1>');
+        return res.end();
+    }
+    if(req.url === '/about') {
+        res.write('<h1>about</h1>');
+        return res.end();
+    }
+
+    res.write('<h1>404 Not Found</h1>');
     res.end();
-}).listen(3001);
+})
+server.listen(3001);
 
 console.log('Server listening on port 3001')
